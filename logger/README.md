e813b7fd32b3317495832d5f37b67f66c6be9a01
jerry <smg@sightp.com>
2019-03-28 11:22:05 +0800

    增加日志输出方法

---
+++ logger/README.md
@@ -0,0 +1,38 @@
+### 日志方法使用说明
+
+按不同的日志级别(TRACE < DEBUG < INFO < WARN < ERROR), 使用不同的颜色输出到控制台.
+并可以设置将日志保存到文件的级别, 每天日志文件.
+
+1. 配置文件说明
+
+    ```
+    {
+      "level": "DEBUG", // 日志级别(TRACE < DEBUG < INFO < WARN < ERROR),在这个级别以下的,不输出
+      "file": { // 定义需要输出到文件的日志
+        "logPath": "/path/to/log/",    // 日志的保存目录
+        "level": "WARN" // 在这个级别及以上的日志,输出到文件
+      }
+    }
+    ```
+    
+2. 使用说明
+
+    ```
+        // 从配置文件初始化
+        // logger.InitFromJson("/path/to/logger.json")
+        
+        // 使用默认初始化
+        logger.InitDefault()
+        
+        // 输出日志信息
+        logger.Trace("msg from trace")
+        logger.Debug("msg from debug")
+        logger.Info("msg from info")
+        logger.Warn("msg from warn")
+        logger.Error("msg from error")
+        
+        // 在初始化后, 还可以动态的设置输出
+        logger.SetLevel(logger.LEVEL_WARN)
+    ```
+
+    
--- logger/logger.go
+++ logger/logger.go
@@ -1,99 +1,189 @@
 package logger
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
+	"strings"
 	"time"
 )
 
 var (
-	Out              *out
-	logErrorPath     string
-	logErrorFile     *os.File
-	logErrorFilename string
+	flag        = log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile
+	globalLevel = LEVEL_TRACE
+	logFiles    map[int]*os.File
+	logWrites   map[int]*log.Logger
+	levels      map[int]string
+	config      *Config
 )
 
-type out struct {
-	Info  *log.Logger
-	Warn  *log.Logger
-	Error *log.Logger
-}
+// TRACE < DEBUG < INFO < WARN < ERROR
+const (
+	LEVEL_TRACE = iota
+	LEVEL_DEBUG
+	LEVEL_INFO
+	LEVEL_WARN
+	LEVEL_ERROR
+)
 
 const (
-	colorBlack = uint8(iota + 90)
-	colorRed
-	colorGreen
-	colorYellow
-	colorBlue
-	colorMagenta
-	colorCyan
-	colorWhite
+	colorTrace = "1;32"
+	colorDebug = "1;32"
+	colorInfo  = "1;36"
+	colorWarn  = "1;33"
+	colorError = "1;31"
 )
 
-func Init(logPath string) {
-	Out = &out{}
-
-	flag := log.Ldate | log.Ltime | log.Lshortfile
-
-	Out.Info = log.New(os.Stdout, Info(""), flag)
-	Out.Warn = log.New(os.Stdout, Warn(""), flag)
-
-	if logPath == "" {
-		logPath = filepath.Dir(os.Args[0]) + "/log/"
+// 从配置文件中初始化
+func InitFromJson(filename string) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic("init logger config file error " + err.Error())
 	}
 
-	logErrorPath = logPath
+	initLogger(b)
+}
 
-	if _, err := os.Stat(logErrorPath); err != nil {
-		os.Mkdir(logErrorPath, os.ModePerm)
+// 使用默认初始化
+func InitDefault() {
+	str := `{
+			  "level": "DEBUG",
+			  "file": {
+				"logPath": "./log/",
+				"level": "WARN"
+			  }
+			}`
+
+	initLogger([]byte(str))
+}
+
+func initLogger(bytes []byte) {
+	config = &Config{}
+	if err := json.Unmarshal(bytes, config); err != nil {
+		panic("parse logger config error " + err.Error())
 	}
 
-	logErrorFilename = time.Now().Format("20060102")
-	filename := logErrorPath + "error-" + logErrorFilename + ".txt"
-	logErrorFile, _ = os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, os.ModePerm)
-	Out.Error = log.New(io.MultiWriter(logErrorFile, os.Stderr), Error(""), flag)
+	if _, err := os.Stat(config.File.LogPath); err != nil {
+		if err := os.MkdirAll(config.File.LogPath, os.ModePerm); err != nil {
+			panic("create file.logPath error, " + err.Error())
+		}
+	}
+
+	levels = make(map[int]string)
+	levels[LEVEL_TRACE] = "TRACE"
+	levels[LEVEL_DEBUG] = "DEBUG"
+	levels[LEVEL_INFO] = "INFO"
+	levels[LEVEL_WARN] = "WARN"
+	levels[LEVEL_ERROR] = "ERROR"
+
+	logFiles = make(map[int]*os.File)
+	logWrites = make(map[int]*log.Logger)
+
+	initLoggerWrite()
 
 	checkDay()
 }
 
+// 每天生成一个文件
 func checkDay() {
 	go func() {
 		for range time.Tick(1 * time.Minute) {
-			if filename := time.Now().Format("20060102"); filename != logErrorFilename {
-				logErrorFile.Close()
-				logErrorFilename = filename
-
-				filename := logErrorPath + "error-" + logErrorFilename + ".txt"
-				logErrorFile, _ = os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, os.ModePerm)
-				Out.Error.SetOutput(io.MultiWriter(logErrorFile, os.Stderr))
-			}
+			initLoggerWrite()
 		}
 	}()
 }
 
-func Trace(msg string) string {
-	return fmt.Sprintf("\x1b[%dm[%s] %s\x1b[0m", colorCyan, "TRACE", msg)
+func initLoggerWrite() {
+	// 关闭所有已打开的文件
+	for _, file := range logFiles {
+		file.Close()
+	}
+
+	// 输出到文件的最低级别
+	minFileLevel := getMinFileLevel()
+
+	// 创建日志输出的文件
+	var err error
+	date := time.Now().Format("20060102")
+	for i, level := range levels {
+		if i >= minFileLevel {
+			filename := fmt.Sprintf("%s/%s_%s.log", config.File.LogPath, strings.ToLower(level), date)
+			logFiles[i], err = os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, os.ModePerm)
+
+			if err != nil {
+				panic("create log file error: " + filename)
+			}
+
+			logWrites[i] = log.New(io.MultiWriter(logFiles[i], os.Stdout), "", flag)
+		} else {
+			logWrites[i] = log.New(os.Stdout, "", flag)
+		}
+	}
 }
 
-func Error(msg string) string {
-	return fmt.Sprintf("\x1b[%dm[%s] %s\x1b[0m", colorRed, "ERROR", msg)
+func getMinFileLevel() int {
+	minFileLevel := LEVEL_WARN
+	for i, level := range levels {
+		if level == config.File.Level {
+			minFileLevel = i
+			break
+		}
+	}
+
+	return minFileLevel
 }
 
-func Warn(msg string) string {
-	return fmt.Sprintf("\x1b[%dm[%s] %s\x1b[0m", colorYellow, "WARN", msg)
+// 动态设置日志输出级别
+func SetLevel(level int) {
+	if level < LEVEL_TRACE || level > LEVEL_ERROR {
+		level = LEVEL_DEBUG
+	}
+	globalLevel = level
 }
 
-func Info(msg string) string {
-	return fmt.Sprintf("\x1b[%dm[%s] %s\x1b[0m", colorGreen, "INFO", msg)
+func GetLevel() int {
+	return globalLevel
 }
 
-func Debug(msg string) string {
-	return fmt.Sprintf("\x1b[%dm[%s] %s\x1b[0m", colorBlue, "DEBUG", msg)
+func Trace(msg string) {
+	if LEVEL_TRACE >= globalLevel {
+		logWrites[LEVEL_TRACE].Println(fmt.Sprintf("\033[%sm[%s] %s\033[0m", colorTrace, "TRACE", msg))
+	}
 }
 
-func Assert(msg string) string {
-	return fmt.Sprintf("\x1b[%dm[%s] %s\x1b[0m", colorMagenta, "ASSERT", msg)
+func Debug(msg string) {
+	if LEVEL_DEBUG >= globalLevel {
+		logWrites[LEVEL_DEBUG].Println(fmt.Sprintf("\033[%sm[%s] %s\033[0m", colorDebug, "DEBUG", msg))
+	}
 }
+
+func Info(msg string) {
+	if LEVEL_INFO >= globalLevel {
+		logWrites[LEVEL_INFO].Println(fmt.Sprintf("\033[%sm[%s] %s\033[0m", colorInfo, "INFO", msg))
+	}
+}
+
+func Warn(msg string) {
+	if LEVEL_WARN >= globalLevel {
+		logWrites[LEVEL_WARN].Println(fmt.Sprintf("\033[%sm[%s] %s\033[0m", colorWarn, "WARN", msg))
+	}
+}
+
+func Error(msg string) {
+	if LEVEL_ERROR >= globalLevel {
+		logWrites[LEVEL_ERROR].Println(fmt.Sprintf("\033[%sm[%s] %s\033[0m", colorError, "ERROR", msg))
+	}
+}
+
+type (
+	Config struct {
+		Level string `json:"level"`
+		File  struct {
+			LogPath string `json:"logPath"`
+			Level   string `json:"level"`
+		} `json:"file"`
+	}
+)
