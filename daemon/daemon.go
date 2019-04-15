53743f75bfa526414a0bfacb09cf2bb4abad4907
jerry <smg@sightp.com>
2019-04-15 12:45:57 +0800

    更新实现

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1,22 +1,31 @@
 package daemon
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 )
 
-func Run() {
-	args := os.Args[1:]
-	for i := 0; i < len(args); i++ {
-		if args[i] == "-d=true" {
-			args[i] = "-d=false"
-			break
-		}
+var daemon = flag.Bool("d", false, "run app as a daemon with -d=true")
+
+func init() {
+	if !flag.Parsed() {
+		flag.Parse()
 	}
 
-	cmd := exec.Command(os.Args[0], args...)
-	cmd.Start()
-	fmt.Println("[PID]", cmd.Process.Pid)
-	os.Exit(0)
+	if *daemon {
+		args := os.Args[1:]
+		for i := 0; i < len(args); i++ {
+			if args[i] == "-d=true" {
+				args[i] = "-d=false"
+				break
+			}
+		}
+
+		cmd := exec.Command(os.Args[0], args...)
+		cmd.Start()
+		fmt.Println("[PID]", cmd.Process.Pid)
+		os.Exit(0)
+	}
 }
